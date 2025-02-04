daf31cf710c0429da3548b3b63d39f0ae2bf8e26
Gabriel Cosmo <bielxx94@gmail.com>
2025-02-04 19:29:53 -0300

    feat: implementa a interrupção do timer de repetição para atualizar os leds

--- Repeating_Timer.c
+++ Repeating_Timer.c
@@ -15,36 +15,40 @@ void init_rgb(const uint *rgb)
     {
         gpio_init(rgb[i]);
         gpio_set_dir(rgb[i], GPIO_OUT);
+        gpio_put(rgb[i], false); //garante que os leds comecem apagados
     }
 }
 
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+        if(gpio_get(rgb_led[0]) && !gpio_get(rgb_led[1]))  //se apenas vermelho estiver ligado
+        {   
+            gpio_put(rgb_led[1], true); //liga amarelo (vermelho + verde)
+        }else if(gpio_get(rgb_led[0]) && gpio_get(rgb_led[1]))  //se amarelo estiver ligado (vermelho e verde)
+        {
+            gpio_put(rgb_led[0], false);    //liga verde (desliga o canal vermelho)
+        }else if(gpio_get(rgb_led[1]) && !gpio_get(rgb_led[0]))    //se apenas verde estiver ligado
+        {
+            gpio_put(rgb_led[1], false);    //desliga verde
+            gpio_put(rgb_led[0], true);     //liga vermelho
+        }
+}
 
 int main()
 {
     stdio_init_all();
     init_rgb(rgb_led);
 
-    // // Timer example code - This example fires off the callback after 2000ms
-    // add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+    struct repeating_timer timer;
+    
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    // printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    // printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+    gpio_put(rgb_led[0], true);
+
 
     while (true) {
         
-        gpio_put(rgb_led[0], true);
-        sleep_ms(400);
-        gpio_put(rgb_led[0], false);
-        sleep_ms(400);
-        gpio_put(rgb_led[1], true);
-        sleep_ms(400);
-        gpio_put(rgb_led[1], false);
-        sleep_ms(400);
-        gpio_put(rgb_led[2], true);
-        sleep_ms(400);
-        gpio_put(rgb_led[2], false);
-        sleep_ms(400);
+        printf("\nLaço principal.");
+        sleep_ms(1000);
     }
 }
